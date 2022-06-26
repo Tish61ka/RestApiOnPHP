5822623ab2e4dd1e95325869b21b94a2deb5ede9
Tishka <yuriitish@mail.ru>
2022-06-26 11:02:52 +0400

    Метод POST довабвление поста

--- back/function.php
+++ back/function.php
@@ -10,6 +10,7 @@
         }
         echo json_encode($postList);
     }
+
     function getPost($connect, $id){
         $post = mysqli_query($connect, "SELECT * FROM `post` WHERE `id` = '$id'");
         if(mysqli_num_rows($post) === 0){
@@ -22,5 +23,17 @@
             $post = mysqli_fetch_assoc($post);
             echo json_encode($post);
         }
+    }
+
+    function addPost($connect, $data){
+        $title = $data['title'];
+        $body = $data['body'];
+        mysqli_query($connect, "INSERT INTO `post` (`title`, `body`) VALUES ('$title', '$body')");
+        http_response_code(201);
+        $res = [
+            "status" => true,
+            "post_id" => mysqli_insert_id($connect)
+        ];
+        echo json_encode($res);
     }
 ?>
